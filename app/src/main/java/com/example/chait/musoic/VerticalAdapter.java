9c46504ffe4054d871b5db8acd173152ad60e544
chaits98 <35635015+chaits98@users.noreply.github.com>
2018-03-10 17:11:10 +0530

    Duration fixed

--- app/src/main/java/com/example/chait/musoic/VerticalAdapter.java
+++ app/src/main/java/com/example/chait/musoic/VerticalAdapter.java
@@ -1,18 +1,13 @@
 package com.example.chait.musoic;
 
-import android.content.Context;
-import android.support.annotation.Nullable;
-import android.support.constraint.ConstraintLayout;
+
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.BaseAdapter;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Created by chait on 07-03-2018.
